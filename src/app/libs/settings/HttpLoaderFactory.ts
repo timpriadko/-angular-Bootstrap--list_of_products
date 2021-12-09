a8c196451c61ac4b55cbee845549e30362351b43
timpriadko <timpriadko@gmail.com>
2021-12-09 19:10:36 +0200

    2 files modified, 2 added, 2 deleted

--- src/app/libs/settings/HttpLoaderFactory.ts
+++ src/app/libs/settings/HttpLoaderFactory.ts
@@ -3,5 +3,5 @@ import { TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
-  return new TranslateHttpLoader(http, 'assets/locale/', '.json');
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
