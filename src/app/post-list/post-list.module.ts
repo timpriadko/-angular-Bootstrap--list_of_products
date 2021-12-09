8d0a6821a724e019d283efec04e0c416ade72f6f
timpriadko <timpriadko@gmail.com>
2021-12-09 19:13:13 +0200

    1 file modified

--- src/app/post-list/post-list.module.ts
+++ src/app/post-list/post-list.module.ts
@@ -1,32 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {
-  MissingTranslationHandler,
-  TranslateLoader,
-  TranslateModule,
-} from '@ngx-translate/core';
-import { HttpLoaderFactory } from '../libs/settings/HttpLoaderFactory';
-import { MissingTranslationService } from '../services/translate-service/MissingTranslationService';
 import { PostListComponent } from './post-list.component';
 import { PostService } from './post.service';
 
 @NgModule({
   declarations: [PostListComponent],
-  imports: [
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-      missingTranslationHandler: {
-        provide: MissingTranslationHandler,
-        useClass: MissingTranslationService,
-      },
-      useDefaultLang: true,
-      defaultLanguage: 'en',
-    }),
-  ],
+  imports: [],
   exports: [PostListComponent],
   providers: [PostService],
 })
