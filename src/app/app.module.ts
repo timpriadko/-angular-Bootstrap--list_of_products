9c185564daa66ccefe3a5648c8b811063992359c
timpriadko <timpriadko@gmail.com>
2021-12-09 15:47:25 +0200

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import {
+  MissingTranslationHandler,
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+import { MissingTranslationService } from './services/translate-service/MissingTranslationService';
+import { HttpLoaderFactory } from './libs/settings/HttpLoaderFactory';
 
 @NgModule({
   declarations: [AppComponent, PostListComponent],
-  imports: [BrowserModule, HttpClientModule, NgbModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      missingTranslationHandler: {
+        provide: MissingTranslationHandler,
+        useClass: MissingTranslationService,
+      },
+      useDefaultLang: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
