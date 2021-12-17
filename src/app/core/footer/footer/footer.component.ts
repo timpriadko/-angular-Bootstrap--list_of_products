d4e527c7ca8eea717322514be0e0e8795f14714b
timpriadko <timpriadko@gmail.com>
2021-12-17 11:46:21 +0200

    6 files modified

--- src/app/core/footer/footer/footer.component.ts
+++ src/app/core/footer/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  constructor() {}
+  constructor(public translateService: TranslateService) {
+    translateService.addLangs(['en', 'fr']);
+    translateService.setDefaultLang('en');
+
+    const browserLang = translateService.getBrowserLang();
+    translateService.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  }
 }
