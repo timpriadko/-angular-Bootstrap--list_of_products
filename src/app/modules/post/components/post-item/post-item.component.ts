092f964fa678591c9d0e712551b60398bb9429fa
timpriadko <timpriadko@gmail.com>
2021-12-17 12:07:30 +0200

    2 files modified

--- src/app/modules/post/components/post-item/post-item.component.ts
+++ src/app/modules/post/components/post-item/post-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../../../../../environments/environment';
 import { Post } from '../../post.service';
@@ -14,6 +14,13 @@ export class PostItemComponent {
 
   @Input()
   index: number;
+
+  @Output()
+  removePostEvent = new EventEmitter<number>();
+
+  removePost(id: number): void {
+    this.removePostEvent.emit(id);
+  }
 
   constructor(public translateService: TranslateService) {
     translateService.addLangs(['en', 'fr']);
