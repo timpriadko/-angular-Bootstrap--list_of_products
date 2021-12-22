bad212030c3f6bbe5f52131c52d1153a9a3395d2
timpriadko <timpriadko@gmail.com>
2021-12-22 15:10:21 +0200

    1 file modified

---
+++ src/app/core/header/components/language-selector/language-selector.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss'],
+})
+export class LanguageSelectorComponent {
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/fr|fr/) ? 'fr' : 'en');
+  }
+}
