a0d43e03b4d2c168c2c18574ab422ab66058f911
timpriadko <timpriadko@gmail.com>
2022-01-06 10:04:34 +0200

    4 files modified

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -2,3 +2,7 @@ export enum Language {
   EN = 'en',
   FR = 'fr',
 }
+
+export enum Copyright {
+  TEXT = 'Copyright © 2022. All rights reserved.',
+}
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Copyright } from '../../core/constants';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  copyrightText: string = Copyright.TEXT;
+}
